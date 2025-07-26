d095d8eb01425ada1c87a611db62845f93e37922
soudelor01 <fengruoxuan@ruc.edu.cn>
2025-07-27 01:24:41 +0800

    update

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ if test $dataset = "Userbehavior";then
   echo 'Data ready!'
   echo 'Data ready!' >> log.txt
   python main_user.py >> log.txt
-  python main_user_task.py >> log.txt
+  python main_user_mgipf.py >> log.txt
 elif test $dataset = "Taobao";then
   python old_gen_0.py > preprocess1.txt 2>&1
   python old_gen_1.py > preprocess2.txt 2>&1
@@ -18,13 +18,13 @@ elif test $dataset = "Taobao";then
   echo 'Data ready!'
   echo 'Data ready!' >> log.txt
   python main_taobao.py >> log.txt
-  python main_taobao_task.py >> log.txt
+  python main_taobao_mgipf.py >> log.txt
 elif test $dataset = "IJCAI";then
   python process_ijcai.py > preprocess.txt 2>&1
   echo 'Data ready!'
   echo 'Data ready!' >> log.txt
   python main_ijcai.py >> log.txt
-  python main_ijcai_task.py >> log.txt
+  python main_ijcai_mgipf.py >> log.txt
 else
   echo 'Wrong Datset!'
 fi
